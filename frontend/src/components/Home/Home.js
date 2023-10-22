7fcb94328a698d6ed69340038157ae7bdf05a100
Karim Salem <karimsalem25@gmail.com>
2023-10-22 22:48:36 +0300

    Added CV upload
    added CV upload for frontend

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
 import { AuthUser } from '../AuthRouter'
 import Navbar from '../Navbar/Navbar'
-
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
 
+  const [file, setFile] = useState(null); 
   const auth = AuthUser()
   useEffect(() => {
     auth.checkUser()
   }, [])
+
+  function handleFileUpload(e) {
+    setFile(e.target.files[0]);
+  }
 
   return (
     <div>
@@ -16,6 +20,9 @@ export default function Home() {
       <div style={{ display: 'flex', 'justifyContent': 'center' }}>
         <h1 >User Name : {(auth.user) ? (auth.user.data.name) : "No User!"}</h1>
       </div>
+      <input type="file" onChange={handleFileUpload}/>
+
+      <button>Upload File</button>
     </div>
   )
 }
